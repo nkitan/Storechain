b4c473c8f9aad2ad5b698108c1a8d65250559430
Ankit Das <ankitdas@protonmail.com>
2022-08-25 15:40:50 +0530

    Fix main.cpp
    Make code more comprehensive

--- main.cpp
+++ main.cpp
@@ -3,13 +3,15 @@
 int main(){
     storinator blockchain;
     
+    UNIT_DATA_FORMAT AD("SatoshiNakamoto, AnkitDas", 9);
+    UNIT_DATA_FORMAT AB("AkshatBakshi, AnkitDas", 9);
+    
     // UNIT_DATA_FORMAT and UDF can be used interchangeably
-    UNIT_DATA_FORMAT wa("SatoshiNakamoto, AnkitDas", 9);
-    UDF sa("Hehe, AnkitDas", 100);
-    UDF satik("SARIK", 1);
+    UDF sa("AnkitKumar, AnkitDas", 100);
+    UDF abc("ABC", 1);
 
     // UDF objects can be added to the transaction list of a unit
-    blockchain.addUnit({wa, sa, satik});
+    blockchain.addUnit({AD,AB, sa, abc, satik});
 
     // showAllUnits can be used to display all Units in the blockchain
     blockchain.showAllUnits();
